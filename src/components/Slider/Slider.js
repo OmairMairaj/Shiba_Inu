86b7e3e9d6459bfd941de2d68fcaafddafdc01a9
haris316 <gogeto931@gmail.com>
2021-11-23 21:35:12 +0500

    Google Maps Done

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -4,6 +4,7 @@ function Slider(data) {
   const slider = () => {
     return data.data.map((marker) => (
       <div className="oneBox">
+        {(marker.pictures.length!==0) ? <img className="onePicture" src={`${marker.pictures[0]}`}/>:<div className="NoPicture">No Image</div>}
         <p className="oneText">Name : {marker.name}</p>
         <p className="oneText">Address : {marker.address}</p>
       </div>
