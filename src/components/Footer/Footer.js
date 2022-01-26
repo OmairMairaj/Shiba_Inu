0c9280bcf3651acd0a6610a60986b4ae2f91bb01
haris316 <gogeto931@gmail.com>
2022-01-26 19:51:22 +0500

    Footer Done

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,17 @@
 import React from "react";
 import "./Footer.css";
+import facebook from "../../assets/facebook.png"
+import instagram from "../../assets/instagram.png"
+import twitter from "../../assets/twitter.png"
 
 function Footer() {
   return (
-    <div className="footer">
-      <div className="footer__section"></div>
+    <div className="container">
+      <div className="social_icons">
+        <img className="one_icon" src={facebook} />
+        <img className="one_icon" src={instagram} />
+        <img className="one_icon" src={twitter} />
+      </div>
     </div>
   );
 }
